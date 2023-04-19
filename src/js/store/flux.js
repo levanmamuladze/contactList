97a9aec7871988888ab111b463bae407054ebbcd
levanmamuladze <levanmamoulatze@gmail.com>
2023-04-19 19:10:32 +0000

    new contact is working

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,10 +1,27 @@
+import { json } from "react-router";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			
-			
+			contacts:[],
+			newContact:{}
 		},
 		actions: {
+			getAllContacts: async  ()=>{
+				const response = await fetch ("https://assets.breatheco.de/apis/fake/contact/agenda/levan-claudio");
+				const data = await response.json();
+				setStore({contacts: data});
+			},
+			addContact: async (newContact) => {
+				console.log(newContact);
+				const response = await fetch("https://assets.breatheco.de/apis/fake/contact/", {
+				  method: "POST",
+				  headers: { "Content-Type": "application/json" },
+				  body: JSON.stringify(newContact),
+				});
+				const data = await response.json();
+				setStore({ contacts: [...getStore().contacts, data] });
+			  },
 			
 
 		}
