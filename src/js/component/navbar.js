35e6b244b7137ebd0aadc67f760266396a897f60
CL4UD3PT <cifpinho@gmail.com>
2023-04-20 22:27:23 +0000

    navbar icon and logo now vertically centered

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -7,8 +7,8 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3 p-3">
 			<div className="container">
-			<Link to="/" className="navbar-brand">
-				<span><i class="fa-solid fa-address-card px-2"></i>CONTACT LIST</span>
+			<Link to="/" className="navbar-brand d-flex align-items-center">
+				<i className="fa-solid fa-address-card px-2 fs-2"></i><span>CONTACT LIST</span>
 			</Link>
 			<div className="ml-auto">
 				{location.pathname === "/" 
