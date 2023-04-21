c3c10624584de0356a1b68bb49b594169049be30
levanmamuladze <levanmamoulatze@gmail.com>
2023-04-21 13:13:45 +0000

    floating input and check for valid email done

--- src/js/views/manage-contact.js
+++ src/js/views/manage-contact.js
@@ -3,91 +3,147 @@ import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const ManageContact = () => {
-    const { store, actions } = useContext(Context);
-    const [contactData, setContactData] = useState({});
-    const params = useParams();
-    const [buttonClicked, setButtonClicked] = useState(false);
+  const { store, actions } = useContext(Context);
+  const [contactData, setContactData] = useState({});
+  const params = useParams();
+  const [buttonClicked, setButtonClicked] = useState(false);
 
-    useEffect(() => {
-        if(params.id) setContactData(getContact());
-    }, [])
+  useEffect(() => {
+    if (params.id) setContactData(getContact());
+  }, []);
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
 
-    const handleAddContact = () => {
-        if(params.id){
-            actions.updateContact(contactData);    
-        }else{
-            actions.addContact(contactData);
-        }
-        setButtonClicked(true);
+  const handleAddContact = () => {
+    if (params.id) {
+      actions.updateContact(contactData);
+    } else {
+      actions.addContact(contactData);
     }
-    
-    const getContact = async () => {
-        const response = await fetch("https://assets.breatheco.de/apis/fake/contact/" + params.id);
-        const data = await response.json();
-        setContactData(data);
-    }
+    setButtonClicked(true);
+  };
 
-    return (
-        <div className="container">
-            <div className="row">
-                <h1>{params.id ? "Edit contact" : "Add a new contact"}</h1>
-            </div>
-            <label htmlFor="name" className="form-label">Name:</label>
-            <input type="text" className="form-control" id="name"
-                value={contactData.full_name || ""}
-                onChange={(e) =>
-                    setContactData({ ...contactData, full_name: e.target.value })
-                }
-                onFocus={()=>{setButtonClicked(false)}}
-            />
-
-            <label htmlFor="email" className="form-label">Email address:</label>
-            <input type="email" className="form-control" id="email"
-                value={contactData.email || ""}
-                onChange={(e) =>
-                    setContactData({ ...contactData, email: e.target.value })
-                }
-                onFocus={()=>{setButtonClicked(false)}}
-            />
-
-            <label htmlFor="address" className="form-label">Address:</label>
-            <input type="text" className="form-control" id="address"
-                value={contactData.address || ""}
-                onChange={(e) =>
-                    setContactData({ ...contactData, address: e.target.value })
-                }
-                onFocus={()=>{setButtonClicked(false)}}
-            />
-
-            <label htmlFor="phone" className="form-label">Phone:</label>
-            <input type="number" className="form-control" id="phone"
-                value={contactData.phone || ""}
-                onChange={(e) =>
-                    setContactData({ ...contactData, phone: e.target.value.toString() })
-                }
-                onFocus={()=>{setButtonClicked(false)}}
-            />
-
-            <label htmlFor="agenda" className="form-label">Agenda:</label>
-            <select id="agenda" className="form-select" aria-label="Default select example"
-                onChange={(e) =>
-                    setContactData({ ...contactData, agenda_slug: e.target.value })
-                }
-                onFocus={()=>{setButtonClicked(false)}}
-            >
-                <option >Select an Agenda</option>
-                <option value={contactData.agenda_slug || "levan-claudio"} selected={contactData.agenda_slug}>{contactData.agenda_slug || "levan-claudio"}</option>
-            </select>
-            
-            <div className="row my-3">
-            <Link >
-                <button type="submit" className="btn btn-primary w-100" onClick={handleAddContact} disabled={buttonClicked}>{params.id?"Save" : "Add"}</button>
-            </Link>
-
-            <Link to="/">
-                <span className="">or get back to home page</span>
-            </Link>
-            </div>
-        </div>
+  const getContact = async () => {
+    const response = await fetch(
+      "https://assets.breatheco.de/apis/fake/contact/" + params.id
     );
+    const data = await response.json();
+    setContactData(data);
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>{params.id ? "Edit contact" : "Add a new contact"}</h1>
+      </div>
+      <div className="form-floating mb-3">
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          placeholder="Name"
+          value={contactData.full_name || ""}
+          onChange={(e) =>
+            setContactData({ ...contactData, full_name: e.target.value })
+          }
+          onFocus={() => {
+            setButtonClicked(false);
+          }}
+        />
+        <label htmlFor="name">Name:</label>
+      </div>
+
+      <div className="form-floating mb-3">
+  <input
+    type="email"
+    className={`form-control ${contactData.email && !isValidEmail(contactData.email) ? "is-invalid" : ""}`}
+    id="email"
+    placeholder="Email address"
+    value={contactData.email || ""}
+    onChange={(e) => setContactData({ ...contactData, email: e.target.value })}
+    onFocus={() => setButtonClicked(false)}
+  />
+  <label htmlFor="email">Email address:</label>
+  {contactData.email && !isValidEmail(contactData.email) && (
+    <div className="invalid-feedback">Please enter a valid email address.</div>
+  )}
+</div>
+
+      <div className="form-floating mb-3">
+        <input
+          type="text"
+          className="form-control"
+          id="address"
+          placeholder="Address"
+          value={contactData.address || ""}
+          onChange={(e) =>
+            setContactData({ ...contactData, address: e.target.value })
+          }
+          onFocus={() => {
+            setButtonClicked(false);
+          }}
+        />
+        <label htmlFor="address">Address:</label>
+      </div>
+
+      <div className="form-floating mb-3">
+        <input
+          type="number"
+          className="form-control"
+          id="phone"
+          placeholder="Phone"
+          value={contactData.phone || ""}
+          onChange={(e) =>
+            setContactData({ ...contactData, phone: e.target.value.toString() })
+          }
+          onFocus={() => {
+            setButtonClicked(false);
+          }}
+        />
+        <label htmlFor="phone">Phone:</label>
+      </div>
+
+      <div className="form-floating mb-3">
+        <select
+          id="agenda"
+          className="form-select"
+          aria-label="Default select example"
+          onChange={(e) =>
+            setContactData({ ...contactData, agenda_slug: e.target.value })
+          }
+          onFocus={() => {
+            setButtonClicked(false);
+          }}
+        >
+          <option>Select an Agenda</option>
+          <option
+            value={contactData.agenda_slug || "levan-claudio"}
+            selected={contactData.agenda_slug}
+          >
+            {contactData.agenda_slug || "levan-claudio"}
+          </option>
+        </select>
+        <label htmlFor="agenda">Agenda:</label>
+      </div>
+
+      <div className="row my-3">
+        <Link>
+          <button
+            type="submit"
+            className="btn btn-primary w-100"
+            onClick={handleAddContact}
+            disabled={buttonClicked}
+          >
+            {params.id ? "Save" : "Add"}
+          </button>
+        </Link>
+
+        <Link to="/">
+          <span className="">or get back to home page</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
