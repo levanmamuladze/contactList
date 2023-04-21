347cc0dd619362d440aedff5fb5f30bcf3fc7612
CL4UD3PT <cifpinho@gmail.com>
2023-04-21 14:58:40 +0000

    contact name to delete shows on modal title

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  // const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [contactToDelete, setContactToDelete] = useState(null);
 
   const handleDeleteContact = (contact) => {
@@ -52,14 +52,14 @@ export const Home = () => {
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">Delete contact</h1>
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Delete contact: {contactToDelete?contactToDelete.full_name:null}</h1>
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
             You really want to delete this lovely person?
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" onClick={() => setShowDeleteModal(false)} data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
               <button className="btn btn-danger" onClick={() => confirmDeleteContact()}>Delete</button>
             </div>
           </div>
